179c344ca312ca915aad32dc9e3eb094d7fe10fa
Ngin Phan <ptncafe2401@yahoo.com>
2019-09-21 23:36:29 +0700

    Ptncafe.ConsoleApp.Test.RabbitMq\Model\TextMessage.cs

---
+++ Ptncafe.ConsoleApp.Test.RabbitMq/Model/TextMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ptncafe.ConsoleApp.Test.RabbitMq.Model
+{
+    public class TextMessage
+    {
+        public int Index { get; set; }
+        public System.DateTime CreatedDate { get; set; }
+        public string Message { get; set; }
+
+    }
+}
---
+++ Ptncafe.ConsoleApp.Test.RabbitMq/Program.cs
@@ -0,0 +1,73 @@
+﻿using EasyNetQ;
+using Ptncafe.ConsoleApp.Test.RabbitMq.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ptncafe.ConsoleApp.RabbitMq
+{
+    /// <summary>
+    /// https://reindeer.rmq.cloudamqp.com/#/
+    /// </summary>
+    internal class Program
+    {
+        private static string _hostConnectString = "amqp://unnshbpd:U3dN66ZQ8GY0muSa3HWp9XRR1keAVI4x@reindeer.rmq.cloudamqp.com/unnshbpd";
+
+        private static async Task Main(string[] args)
+
+        {
+            string input = ReadLine.Read("Publish yes/no ");
+            Console.WriteLine();
+
+            if (input.Equals("no"))
+            {
+
+
+                Console.ReadLine();
+            }
+
+            input = ReadLine.Read("Publish max size? ");
+            using (var rabbitMqClient = new RabbitMqClient(_hostConnectString))
+            {
+                await rabbitMqClient.TestPublish(Convert.ToInt32(input));
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    public class RabbitMqClient : System.IDisposable
+    {
+        private string _hostConnectString;//= ;
+        private IBus _bus;
+
+        public RabbitMqClient(string hostConnectString)
+        {
+            _hostConnectString = hostConnectString;
+            _bus = RabbitHutch.CreateBus(_hostConnectString);
+        }
+
+        public void Dispose()
+        {
+            _bus.Dispose();
+        }
+
+        public async Task TestPublish(int maxSize)
+        {
+            List<Task> tasks = new List<Task>();
+            for (int i = 0; i < maxSize; i++)
+            {
+                var message = new TextMessage
+                {
+                    CreatedDate = DateTime.Now,
+                    Index = i,
+                    Message = $"TestPublish_{i}_{DateTime.Now}"
+                };
+                Console.WriteLine(ObjectDumper.Dump(message));
+
+                tasks.Add(_bus.PublishAsync(message));
+            }
+            await Task.WhenAll(tasks);
+        }
+    }
+}
